5d38b03e7b71b3bf2781d74decc073b8bdaf9957
Behrang Noruzi Niya <behrangn@gmail.com>
2014-07-25 20:51:56 +0430

    Improve code format

--- jalaali.hs
+++ jalaali.hs
@@ -7,6 +7,11 @@ type DayInMarch = Int
 -- | An Int representing number of years since the last leap year.
 type LeapOffset = Int
 
+-- Jalaali years starting the 33-year rule.
+breaks =  [ -61, 9, 38, 199, 426, 686, 756, 818, 1111, 1181, 1210
+          , 1635, 2060, 2097, 2192, 2262, 2324, 2394, 2456, 3178
+          ]
+firstJump = head (tail breaks) - head breaks -- first jump in breaks
 
 {-|
   This function determines if the Jalaali (Persian) year is
@@ -24,19 +29,16 @@ type LeapOffset = Int
 -}
 jalCal :: JalaaliYear -> (LeapOffset, GregorianYear, DayInMarch)
 jalCal jy
-  | jy < (-61) = error ("invalid jalaali year " ++ show jy ++ ", should be greater than -61")
-  | jy > 3177 = error ("invalid jalaali year " ++ show jy ++ ", should be less than 3177")
+  | jy <= (-62) = error ("invalid jalaali year " ++ show jy ++ ", should be greater than -62")
+  | jy >= 3178 = error ("invalid jalaali year " ++ show jy ++ ", should be less than 3178")
   | otherwise =
-      let breaks =  [ -61, 9, 38, 199, 426, 686, 756, 818, 1111, 1181, 1210
-                    , 1635, 2060, 2097, 2192, 2262, 2324, 2394, 2456, 3178
-                    ] -- Jalaali years starting the 33-year rule.
-          gy = jy + 621
+      let gy = jy + 621
 
           (before, _) = break (jy<) breaks
           n = jy - last before
 
           jumps = zipWith (-) (drop 1 before) before
-          lastJump = last jumps
+          lastJump = last $ firstJump : jumps
 
           -- Find the limiting years for the Jalaali year jy.
           leapJ' = foldl  (\acc jump -> acc +
@@ -46,7 +48,7 @@ jalCal jy
           -- Find the number of leap years from AD 621 to the beginning
           -- of the current Jalaali year in the Persian calendar.
           leapJ'' = leapJ' + ((n `quot` 33) * 8) + (((n `mod` 33) + 3) `quot` 4)
-          leapJ = leapJ'' + if ((lastJump `mod` 33) == 4) && ((lastJump - n) == 4) then 1 else 0
+          leapJ = leapJ'' + if (lastJump `mod` 33) == 4 && (lastJump - n) == 4 then 1 else 0
 
           -- And the same in the Gregorian calendar (until the year gy).
           leapG = (gy `quot` 4) - ((((gy `quot` 100) + 1) * 3) `quot` 4) - 150
@@ -55,7 +57,7 @@ jalCal jy
           dayInMarch = 20 + leapJ - leapG
 
           -- Find how many years have passed since the last leap year.
-          n' = n + if (lastJump - n < 6) then (-lastJump) + (((lastJump + 4) `quot` 33) * 33) else 0
+          n' = n + if lastJump - n < 6 then (-lastJump) + (((lastJump + 4) `quot` 33) * 33) else 0
           leap' = (((n' + 1) `mod` 33) - 1) `mod` 4
           leap = if leap' == -1 then 4 else leap'
 
