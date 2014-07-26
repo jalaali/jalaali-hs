f596e93f55cbe7f4ddb52bb193ffba2f64eb8823
Behrang Noruzi Niya <behrangn@gmail.com>
2014-07-26 14:06:36 +0430

    Add julian day number conversion functions

--- jalaali.hs
+++ jalaali.hs
@@ -1,9 +1,27 @@
 -- | An Int representing a Jalaali year.
 type JalaaliYear = Int
+
+-- | An Int representing a Jalaali month (1-based).
+type JalaaliMonth = Int
+
+-- | An Int representing a Jalaali day.
+type JalaaliDay = Int
+
 -- | An Int representing a Gregorian year.
 type GregorianYear = Int
+
+-- | An Int representing a Gregorian month (1-based).
+type GregorianMonth = Int
+
+-- | An Int representing a Gregorian day.
+type GregorianDay = Int
+
+-- | An Int representing a Julian Day Number
+type JulianDayNumber = Int
+
 -- | An Int representing the day in March when converting a Jalaali year to Gregorian.
 type DayInMarch = Int
+
 -- | An Int representing number of years since the last leap year.
 type LeapOffset = Int
 
@@ -29,8 +47,8 @@ firstJump = head (tail breaks) - head breaks -- first jump in breaks
 -}
 jalCal :: JalaaliYear -> (LeapOffset, GregorianYear, DayInMarch)
 jalCal jy
-  | jy <= (-62) = error ("invalid jalaali year " ++ show jy ++ ", should be greater than -62")
-  | jy >= 3178 = error ("invalid jalaali year " ++ show jy ++ ", should be less than 3178")
+  | jy < (-61) = error ("invalid jalaali year " ++ show jy ++ ", should be >= -61")
+  | jy > 3177 = error ("invalid jalaali year " ++ show jy ++ ", should be <= 3177")
   | otherwise = (leap, gy, dayInMarch)
     where
       gy = jy + 621
@@ -62,3 +80,89 @@ jalCal jy
       n' = n + if lastJump - n < 6 then (-lastJump) + quot33 (lastJump + 4) * 33 else 0
       leap' = (mod33 (n' + 1) - 1) `mod` 4
       leap = if leap' == -1 then 4 else leap'
+
+{-|
+  Converts a date of the Jalaali calendar to the Julian Day number.
+
+  @param jy Jalaali year (1 to 3100)
+  @param jm Jalaali month (1 to 12)
+  @param jd Jalaali day (1 to 29/31)
+  @return Julian Day number
+-}
+j2d :: (JalaaliYear, JalaaliMonth, JalaaliDay) -> JulianDayNumber
+j2d (jy, jm, jd) = jdn + (jm - 1) * 31 - (jm `quot` 7) * (jm - 7) + jd - 1
+  where
+    (leap, gy, dayInMarch) = jalCal jy
+    jdn = g2d (gy, 3, dayInMarch)
+
+{-|
+  Converts the Julian Day number to a date in the Jalaali calendar.
+
+  @param jdn Julian Day number
+  @return
+    jy: Jalaali year (1 to 3100)
+    jm: Jalaali month (1 to 12)
+    jd: Jalaali day (1 to 29/31)
+-}
+d2j :: JulianDayNumber -> (JalaaliYear, JalaaliMonth, JalaaliDay)
+d2j jdn = (jy, jm, jd)
+  where
+    (gy, _, _) = d2g jdn
+    jy' = gy - 621
+    (leap, _, dayInMarch) = jalCal jy'
+    jdn1f = g2d (gy, 3, dayInMarch)
+    k' = jdn - jdn1f
+    k = if k' >= 0 && k' <= 185
+        then k'
+        else if k' >= 0
+          then k' - 186
+          else k' + 179 + if leap == 1 then 1 else 0
+    jy = jy' - if k' < 0 then 1 else 0 -- Previous Jalaali year.
+    jm =  if k' >= 0 && k' <= 185
+          then 1 + (k `quot` 31)
+          else 7 + (k `quot` 30)
+    jd =  if k' >= 0 && k' <= 185
+          then 1 + (k `mod` 31)
+          else 1 + (k `mod` 30)
+
+
+{-|
+  Calculates the Julian Day number from Gregorian or Julian
+  calendar dates. This integer number corresponds to the noon of
+  the date (i.e. 12 hours of Universal Time).
+  The procedure was tested to be good since 1 March, -100100 (of both
+  calendars) up to a few million years into the future.
+
+  @param gy Calendar year (years BC numbered 0, -1, -2, ...)
+  @param gm Calendar month (1 to 12)
+  @param gd Calendar day of the month (1 to 28/29/30/31)
+  @return Julian Day number
+-}
+g2d :: (GregorianYear, GregorianMonth, GregorianDay) -> JulianDayNumber
+g2d (gy, gm, gd) =
+  d - ((((gy + 100100 + ((gm - 8) `quot` 6)) `quot` 100) * 3) `quot` 4) + 752
+  where
+    d = ((gy + ((gm - 8) `quot` 6) + 100100) * 1461) `quot` 4 +
+        (153 * ((gm + 9) `mod` 12) + 2) `quot` 5 +
+        gd - 34840408
+
+{-|
+  Calculates Gregorian and Julian calendar dates from the Julian Day number
+  (jdn) for the period since jdn=-34839655 (i.e. the year -100100 of both
+  calendars) to some millions years ahead of the present.
+
+  @param jdn Julian Day number
+  @return
+    gy: Calendar year (years BC numbered 0, -1, -2, ...)
+    gm: Calendar month (1 to 12)
+    gd: Calendar day of the month M (1 to 28/29/30/31)
+-}
+d2g :: JulianDayNumber -> (GregorianYear, GregorianMonth, GregorianDay)
+d2g jdn = (gy, gm, gd)
+  where
+    j' = 4 * jdn + 139361631
+    j = j' + ((((4 * jdn + 183187720) `quot` 146097) * 3) `quot` 4) * 4 - 3908
+    i = ((j `mod` 1461) `quot` 4) * 5 + 308
+    gd = ((i `mod` 153) `quot` 5) + 1
+    gm = ((i `quot` 153) `mod` 12) + 1
+    gy = (j `quot` 1461) - 100100 + ((8 - gm) `quot` 6)
